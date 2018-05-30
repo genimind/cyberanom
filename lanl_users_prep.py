0c97e963a4c5d0eae7d5c2bdd97dd0bf50de8614
isallam <ibe.sallam@gmail.com>
2018-05-30 19:43:26 -0400

    fix indentation issue

--- lanl_users_prep.py
+++ lanl_users_prep.py
@@ -26,24 +26,24 @@ def process_user(infile_name, outfile_name):
         "sec, day, red, sent_len, sentence" which is used as an input to the model
     '''
     with open(infile_name, 'r') as infile, open(outfile_name, 'w') as outfile:
-    outfile.write('sec,day,red,seq_len,sentence\n') # header
-    redcount = 0
-    for line in infile.readlines():
-        line = line.strip().split(',')
-        sentence = ','.join(line[1:])
-        diff = max_len - len(sentence)
-        sec = line[0]
-        day = math.floor(int(sec)/86400)
-        red = 0
-        redentry = "{0},{1},{2},{3}".format(line[0], line[1], line[3], line[4])
-        red += int(redentry in redevents) # 1 if line is red event
-        redcount += red
-        translated = transform_line(sentence)
-        outfile.write("%s,%s,%s,%s,%s\n" % (sec, day, 
-                                                red, len(sentence), translated))
-    print('done - red team events:', redcount)
-    outfile.close()
-    infile.close()
+        outfile.write('sec,day,red,seq_len,sentence\n') # header
+        redcount = 0
+        for line in infile.readlines():
+            line = line.strip().split(',')
+            sentence = ','.join(line[1:])
+            diff = max_len - len(sentence)
+            sec = line[0]
+            day = math.floor(int(sec)/86400)
+            red = 0
+            redentry = "{0},{1},{2},{3}".format(line[0], line[1], line[3], line[4])
+            red += int(redentry in redevents) # 1 if line is red event
+            redcount += red
+            translated = transform_line(sentence)
+            outfile.write("%s,%s,%s,%s,%s\n" % (sec, day, 
+                                                    red, len(sentence), translated))
+        print('done - red team events:', redcount)
+        outfile.close()
+        infile.close()
 
 
 def split_to_multiple_days(user_infile, user_outdir):
@@ -52,26 +52,26 @@ def split_to_multiple_days(user_infile, user_outdir):
         directory with the user name. The files are used as an input to the model
     '''
     with open(user_infile, 'r') as data:
-    current_day = 0
-    outfile = open(user_outdir + str(current_day) + '.txt', 'w')
-    print('processing:', u, '...', current_day, end='')
-    data.readline()
-    for line in data.readlines():
-        try:
-            line_items = line.strip().split(',')
-            day = int(line_items[1])
-            if day == current_day:
-                outfile.write(line)
-            else:
-                outfile.close()
-                current_day = day
-                outfile = open(user_outdir + str(current_day) + '.txt', 'w')
-                print(',', current_day, end='')
-                outfile.write(line)
-        except:
-            print('error processing file.... line: ', line)
-    outfile.close()
-    print(' ...Done!')
+        current_day = 0
+        outfile = open(user_outdir + str(current_day) + '.txt', 'w')
+        print('processing:', u, '...', current_day, end='')
+        data.readline()
+        for line in data.readlines():
+            try:
+                line_items = line.strip().split(',')
+                day = int(line_items[1])
+                if day == current_day:
+                    outfile.write(line)
+                else:
+                    outfile.close()
+                    current_day = day
+                    outfile = open(user_outdir + str(current_day) + '.txt', 'w')
+                    print(',', current_day, end='')
+                    outfile.write(line)
+            except:
+                print('error processing file.... line: ', line)
+        outfile.close()
+        print(' ...Done!')
 
 if __name__ == "__main__":
     # make sure we have the output dir
